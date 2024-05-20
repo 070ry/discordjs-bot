0ad3383c2bfac59440fe08d48c78f7c914fca49a
070ry <onewsacount@gmail.com>
2024-05-20 21:04:12 +0900

    chore: フォーマット

--- src/handlers/ClientReady.js
+++ src/handlers/ClientReady.js
@@ -1,6 +1,6 @@
 module.exports = (e, client, config) => {
   const { log } = require("../utils/logger");
   log(
-    `[ Events/ClientReady ] Logged in as ${client.user.tag}! Version: ${config.general.version}`
+    `[ Events/ClientReady ] Logged in as ${client.user.tag}! Version: ${config.general.version}`,
   );
 };
